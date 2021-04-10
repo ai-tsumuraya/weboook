0749435b110b3e2a1036a0837dad3556c213cbf2
ai.tsumuraya <aitsumuraya@gmail.com>
2021-04-10 22:46:01 +0000

    Fix an additional bookmark

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @bookmark = @user.bookmarks.build  
+    # @bookmark = @user.bookmarks.build  
     @bookmarks = @user.bookmarks
   end
 
